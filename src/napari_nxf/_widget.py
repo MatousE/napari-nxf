f21b29b01d7002604f42bb9f48f6896307a622a2
MatousE <matous.elphick@gmail.com>
2023-03-31 16:24:59 +0100

    FEAT - Adding hello world nextflow pipeline

--- src/napari_nxf/_widget.py
+++ src/napari_nxf/_widget.py
@@ -1,12 +1,6 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
-
-Replace code below according to your needs.
-"""
+from pathlib import Path
 from typing import TYPE_CHECKING
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from qtpy.QtWidgets import QVBoxLayout, QPushButton, QWidget
 
 if TYPE_CHECKING:
     import napari
@@ -16,12 +10,35 @@ class ExampleQWidget(QWidget):
     def __init__(self, napari_viewer):
         super().__init__()
         self.viewer = napari_viewer
+        self.setLayout(QVBoxLayout())
 
         btn = QPushButton("Click me!")
         btn.clicked.connect(self._on_click)
 
-        self.setLayout(QHBoxLayout())
         self.layout().addWidget(btn)
 
+    def abspath(self, root, relpath):
+        root = Path(root)
+        if root.is_dir():
+            path = root / relpath
+        else:
+            path = root.parent / relpath
+        return str(path.absolute())
+
     def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        import nextflow
+
+        nxf_path = self.abspath(__file__, 'nextflow/main.nf')
+        nxf_config_path = self.abspath(__file__, 'nextflow/nextflow.config')
+
+        pipeline1 = nextflow.Pipeline(nxf_path, config=nxf_config_path)
+
+        print(nxf_path)
+        print(nxf_config_path)
+        print(pipeline1)
+
+        execution = pipeline1.run()
+
+        print(execution.status)
+
+        print(execution.stdout)
